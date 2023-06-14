7f2632ef955f30215dcc596d475f3ae14c583094
Edwar-Sanz <ssanzjhon5@gmail.com>
2023-06-14 14:52:33 -0500

    solucion error video edad

--- youtube_downloader/descargador.py
+++ youtube_downloader/descargador.py
@@ -7,18 +7,21 @@ folder = "./"           # Carpeta en la que se van a guardar los mp3
 
 
 pl = Playlist(str(url))
-for i in pl.videos:
+for video in pl.videos:
+    
     try:
+        yt = YouTube(video.watch_url, use_oauth=True, allow_oauth_cache=True)
+
         # extract only audio
-        audios = i.streams.filter(only_audio = True).first() 
-        
-        # download the files
-        archivo = audios.download(output_path = folder)
-        
+        audios = yt.streams.filter(only_audio=True).first()
+
+        # download the file
+        archivo = audios.download(output_path=folder)
+
         # save the file
         base, ext = os.path.splitext(archivo)
         convertido = base + '.mp3'
         os.rename(archivo, convertido)
-        
+
     except FileExistsError:
         os.remove(base + '.mp4')
